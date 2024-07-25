2dd4278f3c6beb758a5a62cc4b679cf43c94b0e0
kimsunho2000 <sunhokim28@gmail.com>
2024-07-25 17:02:39 +0900

    add podfile & databasemanger file

---
+++ TwitterClone/Models/TwitterUser.swift
@@ -0,0 +1,8 @@
+//
+//  TwitterUser.swift
+//  TwitterClone
+//
+//  Created by 김선호 on 7/25/24.
+//
+
+import Foundation
---
+++ TwitterClone/Networking/DatabaseManager.swift
@@ -0,0 +1,16 @@
+//
+//  DatabaseMangager.swift
+//  TwitterClone
+//
+//  Created by 김선호 on 7/25/24.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+import FirebaseFirestoreCombineSwift
+
+class DatabaseMangager {
+    
+    static let shared = DatabaseMangager
+}
